6496cb9a24329ba8bd0a2ffe8d1fe42ed3303fec
MkPolo <mk.polo1708@gmail.com>
2020-04-30 12:20:30 -0500

    Se inicializó el proyecto, y conectamos a la base de datos. Close #1

---
+++ src/main/java/com/mkpolo/aplication/AplicationSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.mkpolo.aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mkpolo/aplication/AplicationSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mkpolo.aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
